4dce8b38b6d2a5320c9b35d65074eacde241f44b
Giovane <giovane.barreira@arctouch.com>
2022-07-13 16:44:17 -0300

    create a tableview and detail screen

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Giovane Barreira on 13/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewInterfaceOrientation(.portrait)
+    }
+}
